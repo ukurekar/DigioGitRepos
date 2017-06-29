fdc3056b9d16778564f4a69c81ebc579d8f26407
ukurekar <utkarsha.kurekar@gmail.com>
2017-06-29 16:03:37 +0530

    Commiting Angular1 project

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name digioAssignmentApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the digioAssignmentApp
+ */
+angular.module('digioAssignmentApp')
+  .controller('MainCtrl', function ($scope) {
+    // $scope.email = '123test';
+  });
